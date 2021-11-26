f2c1b1020cc6e65fe3826eb80eb88aff2eb3cb96
Mon <55015209+Mondotosz@users.noreply.github.com>
2021-11-26 12:18:56 +0100

    tried to implement mvvm

--- Droxid/DroxidClient/MainWindow.xaml.cs
+++ Droxid/DroxidClient/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DroxidClient.ViewModels;
 
 namespace DroxidClient
 {
@@ -23,6 +24,7 @@ namespace DroxidClient
         public MainWindow()
         {
             InitializeComponent();
+            MainWindowViewModel vm = new MainWindowViewModel();
         }
 
         private void BtnDroxidClick(object sender, RoutedEventArgs e){
---
+++ Droxid/DroxidClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace DroxidClient.ViewModels
+{
+    public class MainWindowViewModel : ObservableObject
+    {
+        private MainWindow _view;
+
+        public MainWindowViewModel()
+        {
+            _view = new MainWindow();
+        }
+
+        public void inscription(MainWindow mainWindow)
+        {
+            _view = mainWindow;
+        }
+
+    }
+}
